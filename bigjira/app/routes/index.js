c460f334d04d00bd209664238c9bf7f022a28306
Dave Lindsey <10616067+big-wave-dave@users.noreply.github.com>
2018-08-03 11:04:43 -0600

    Update index.js

--- bigjira/app/routes/index.js
+++ bigjira/app/routes/index.js
@@ -5,8 +5,8 @@ const cloneIntoGPM = require('./cloneIntoGPM');
 
 
 module.exports = function(app, jira) {
-    qaCycleRoutes(app, jira);
-    cloneIntoGPM(app, jira);
+    //qaCycleRoutes(app, jira);
+    //cloneIntoGPM(app, jira);
 
     // Other routes can go here in the future
 }
